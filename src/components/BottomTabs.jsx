46d39eacbc7f56c9d8537ea72a404c5dc41fae35
RobbyWahyudi <mazrobby04@gmail.com>
2024-05-03 06:53:50 +0700

    Menambahkan icon pada bottom tabs

--- src/components/BottomTabs.jsx
+++ src/components/BottomTabs.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feather from 'react-native-vector-icons/Feather';
 import Beranda from '../screens/Beranda';
 import Jelajahi from '../screens/Jelajahi';
 import Saya from '../screens/Saya';
@@ -8,7 +10,27 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Beranda"
+      screenOptions={({route}) => ({
+        tabBarStyle: {backgroundColor: '#191717'},
+        tabBarActiveTintColor: 'white',
+        inactiveTintColor: '#dfe7e7',
+        headerShown: false,
+        tabBarIcon: ({color}) => {
+          let iconName;
+
+          if (route.name === 'Beranda') {
+            iconName = 'home';
+          } else if (route.name === 'Saya') {
+            iconName = 'user';
+          } else if (route.name === 'Jelajahi') {
+            iconName = 'headphones';
+          }
+
+          return <Feather name={iconName} size={25} color={color} />;
+        },
+      })}>
       <Tab.Screen name="Beranda" component={Beranda} />
       <Tab.Screen name="Jelajahi" component={Jelajahi} />
       <Tab.Screen name="Saya" component={Saya} />
--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Feather from 'react-native-vector-icons/Feather';
 
 const Beranda = () => {
   return (
